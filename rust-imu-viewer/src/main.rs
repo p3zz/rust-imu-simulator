e2b5579fa8164bc0a1f08dc6a968d215af92b6c7
p3zz <federico.pezzato.work@gmail.com>
2022-04-01 17:15:49 +0200

    add plain cube to scene

--- rust-imu-viewer/src/main.rs
+++ rust-imu-viewer/src/main.rs
@@ -6,24 +6,21 @@ use kiss3d::light::Light;
 use kiss3d::nalgebra::{Vector3, UnitQuaternion, Point3, Point2};
 use kiss3d::text::Font;
 
+const IMU_HEIGHT: f32 = 0.5;
+const IMU_WIDTH: f32 = 1.0;
+const IMU_LENGTH: f32 = 1.0;
+
 fn main() {
     let mut window = Window::new("Kiss3d: cube");
-    let mut cube = window.add_cube(1.0, 1.0, 1.0);
-    let origin: Point3<f32> = Point3::new(0.0, 0.0, 0.0);
-    let red: Point3<f32> = Point3::new(1.0, 0.0, 0.0);
-    let green: Point3<f32> = Point3::new(0.0, 1.0, 0.0);
-    let blue: Point3<f32> = Point3::new(0.0, 0.0, 1.0);
+    let mut c = window.add_cube(IMU_LENGTH, IMU_HEIGHT, IMU_WIDTH);
 
-    cube.set_color(1.0, 0.0, 0.0);
+    c.set_color(1.0, 0.0, 0.0);
 
     window.set_light(Light::StickToCamera);
 
     let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
 
     while window.render() {
-        window.draw_line(&origin, &Point3::new(5.0, 0.0, 0.0), &red);
-        window.draw_line(&origin, &Point3::new(0.0, 5.0, 0.0), &green);
-        window.draw_line(&origin, &Point3::new(0.0, 0.0, 5.0), &blue);
         // c.prepend_to_local_rotation(&rot);
     }
 }
