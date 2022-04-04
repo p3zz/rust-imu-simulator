183adc697a59e11239cfe0af1d7baa20b7d95feb
p3zz <federico.pezzato.work@gmail.com>
2022-04-04 21:39:04 +0200

    use new structs inside main

--- rust-imu-viewer/src/main.rs
+++ rust-imu-viewer/src/main.rs
@@ -12,19 +12,16 @@ use kiss3d::scene::SceneNode;
 use crate::rotation::{IMU_HEIGHT, IMU_LENGTH, IMU_WIDTH, rotate_cube};
 
 fn main() {
-    let mut window = Window::new("Kiss3d: cube");
-    let mut cube = window.add_cube(IMU_LENGTH, IMU_HEIGHT, IMU_WIDTH);
+    let mut graphic_env = rotation::GraphicEnvironment::new("IMU rotation viewer");
+    graphic_env.init();
+    let mut cube = graphic_env.window.add_cube(IMU_LENGTH, IMU_HEIGHT, IMU_WIDTH);
     cube.set_color(1.0, 0.0, 0.0);
-    // let origin: Point3<f32> = Point3::new(0.0, 0.0, 0.0);
-    // let red: Point3<f32> = Point3::new(1.0, 0.0, 0.0);
-    // let green: Point3<f32> = Point3::new(0.0, 1.0, 0.0);
-    // let blue: Point3<f32> = Point3::new(0.0, 0.0, 1.0);
-    window.set_light(Light::StickToCamera);
 
     let (tx, rx) = bounded(0);
     input::spawn_read_line_thread(tx);
 
-    while window.render() {
+    while graphic_env.window.render() {
+        graphic_env.draw_axis();
         let rotation = match rx.try_recv() {
             Ok(msg) => match msg {
                 None => {
