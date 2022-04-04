bbca11798d827fc1697ba038023b0a428dea151e
p3zz <federico.pezzato.work@gmail.com>
2022-04-04 15:58:46 +0200

    use rotate_cube inside main

--- rust-imu-viewer/src/main.rs
+++ rust-imu-viewer/src/main.rs
@@ -9,39 +9,32 @@ use kiss3d::window::Window;
 use kiss3d::light::Light;
 use kiss3d::nalgebra::{Vector3, UnitQuaternion, Point3, Point2};
 use kiss3d::scene::SceneNode;
+use crate::rotation::{IMU_HEIGHT, IMU_LENGTH, IMU_WIDTH, rotate_cube};
 
 fn main() {
-    // let mut window = Window::new("Kiss3d: cube");
-    // let mut cube = window.add_cube(IMU_LENGTH, IMU_HEIGHT, IMU_WIDTH);
+    let mut window = Window::new("Kiss3d: cube");
+    let mut cube = window.add_cube(IMU_LENGTH, IMU_HEIGHT, IMU_WIDTH);
+    cube.set_color(1.0, 0.0, 0.0);
     // let origin: Point3<f32> = Point3::new(0.0, 0.0, 0.0);
     // let red: Point3<f32> = Point3::new(1.0, 0.0, 0.0);
     // let green: Point3<f32> = Point3::new(0.0, 1.0, 0.0);
     // let blue: Point3<f32> = Point3::new(0.0, 0.0, 1.0);
-    //
-    // cube.set_color(1.0, 0.0, 0.0);
-    //
-    // window.set_light(Light::StickToCamera);
-    //
-    // while window.render() {
-    //     window.draw_line(&origin, &Point3::new(5.0, 0.0, 0.0), &red);
-    //     window.draw_line(&origin, &Point3::new(0.0, 5.0, 0.0), &green);
-    //     window.draw_line(&origin, &Point3::new(0.0, 0.0, 5.0), &blue);
-    // }
+    window.set_light(Light::StickToCamera);
 
     let (tx, rx) = bounded(0);
     input::spawn_read_line_thread(tx);
 
-    loop {
-        let line = match rx.try_recv() {
+    while window.render() {
+        let rotation = match rx.try_recv() {
             Ok(msg) => match msg {
                 None => {
                     println!("invalid input line");
                     continue;
                 }
-                Some(rotation) => rotation
+                Some(rot) => rot
             },
             Err(e) => continue
         };
-        println!("{}", line);
+        rotate_cube(&mut cube, rotation);
     }
 }
